4e1923f520ac7776ed7d618d42409ebf340df689
klopez10 <klopez10@eagles.ewu.edu>
2019-05-28 00:00:07 -0700

    Changed and styled to A Quiet Place.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# movie-trailer
+# A Quiet Place
+
+## Resources
+* [IMDb](https://www.imdb.com/title/tt6644200/?ref_=ttfc_fc_tt)
+* [How to Make a Full Screen Landing Page (HTML & CSS)](https://www.youtube.com/watch?v=hExwnLlj2xk)
+* [Smooth Scroll Anchor Links](https://codepen.io/jooleearr/pen/gpooKj)
+* [CSS3 Flickering Neon](https://codepen.io/RMKNGY/pen/ywNVoN)
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
 console.log('OK');
+
+// SCROLLING SECTIONS
+$('.js-anchor-link').click(function(e){
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    if(target.length){
+      var scrollTo = target.offset().top;
+      $('body, html').animate({scrollTop: scrollTo+'px'}, 1000);
+    }
+  });
+  
+// TRAILER
+  const player = new Plyr( '#player' , {
+    controls: ['play', 'progress', 'fullscreen', 'mute']
+  });
+  
+// COPYRIGHT
+  let date = new Date();
+  let currentYear = date.getFullYear();
+  
+  $('footer p span').text( currentYear );
+  
+  console.log( currentYear );
+  console.log( date );
